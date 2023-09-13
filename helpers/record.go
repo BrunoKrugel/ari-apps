f92039f3bad0edd356c4736d3c287508968d7f73
Bruno Krugel <brunokrugel@gmail.com>
2023-09-12 23:43:05 -0300

    create consts

--- helpers/record.go
+++ helpers/record.go
@@ -1,43 +1,47 @@
 package helpers
+
 import (
 	"context"
-	"fmt"
 	"encoding/json"
+	"fmt"
 
+	"github.com/CyCoreSystems/ari/v5"
+	"github.com/google/uuid"
 	"lineblocs.com/processor/api"
 	"lineblocs.com/processor/types"
 	"lineblocs.com/processor/utils"
-	"github.com/google/uuid"
-	"github.com/CyCoreSystems/ari/v5"
 )
 
 type Record struct {
-	Bridge *types.LineBridge
+	Bridge  *types.LineBridge
 	Channel *types.LineChannel
-	User *types.User
-	CallId *int
-	Handle *ari.LiveRecordingHandle
-	Trim bool	
+	User    *types.User
+	CallId  *int
+	Handle  *ari.LiveRecordingHandle
+	Trim    bool
+	Ctx     context.Context
 }
 
 type RecordingParams struct {
-	UserId int `json:"user_id"`
-	CallId *int `json:"call_id"`
-	Tag string `json:"tag"`
-	Status string `json:"status"`
-	WorkspaceId int `json:"workspace_id"`
-	StorageId string `json:"storage_id"`
+	UserId          int    `json:"user_id"`
+	CallId          *int   `json:"call_id"`
+	Tag             string `json:"tag"`
+	Status          string `json:"status"`
+	WorkspaceId     int    `json:"workspace_id"`
+	StorageId       string `json:"storage_id"`
 	StorageServerIp string `json:"storage_server_ip"`
-	Trim bool `json:"trim"`
+	Trim            bool   `json:"trim"`
 }
 
-
-func NewRecording(user *types.User, callId *int, trim bool) (*Record) {
-	record := Record{
-		User: user, CallId:callId, Trim: trim }
-
-	return &record
+func NewRecording(ctx context.Context, user *types.User, callId *int, trim bool) *Record {
+	return &Record{
+		User:   user,
+		CallId: callId,
+		Trim:   trim,
+		Ctx:    ctx,
+	}
 }
+
 func (r *Record) createAPIResource() (string, error) {
 	user := r.User
 	callId := r.CallId
@@ -47,38 +51,35 @@ func (r *Record) createAPIResource() (string, error) {
 		return "", err
 	}
 
-
 	id := uniq.String()
 	params := RecordingParams{
-		UserId: user.Id,
-		CallId:callId,
-		Tag: "",
-		Status: "started",
-		WorkspaceId: user.Workspace.Id,
-		Trim: r.Trim,
-		StorageId: id,
+		UserId:          user.Id,
+		CallId:          callId,
+		Tag:             "",
+		Status:          "started",
+		WorkspaceId:     user.Workspace.Id,
+		Trim:            r.Trim,
+		StorageId:       id,
 		StorageServerIp: utils.GetARIHost()}
 
-
-	body, err := json.Marshal( params )
+	body, err := json.Marshal(params)
 	if err != nil {
-		fmt.Printf( "error occured: %s\r\n", err.Error() )
+		fmt.Printf("error occurred: %s\r\n", err.Error())
 		return "", err
 	}
 
 	fmt.Println("creating recording call...")
-	resp, err := api.SendHttpRequest( "/recording/createRecording", body )
+	resp, err := api.SendHttpRequest("/recording/createRecording", body)
 	if err != nil {
-		fmt.Printf( "error occured: %s\r\n", err.Error() )
+		fmt.Printf("error occurred: %s\r\n", err.Error())
 		return "", err
 	}
 	_ = resp.Headers.Get("x-recording-id")
 	return id, nil
 }
 
-
 func (r *Record) InitiateRecordingForBridge(bridge *types.LineBridge) (string, error) {
-	r.Bridge = bridge 
+	r.Bridge = bridge
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
@@ -89,11 +90,11 @@ func (r *Record) InitiateRecordingForBridge(bridge *types.LineBridge) (string, e
 	}
 
 	key := ari.NewKey(ari.LiveRecordingKey, id)
-	opts := &ari.RecordingOptions{ Format: "wav" }
+	opts := &ari.RecordingOptions{Format: "wav"}
 	hndl, err := bridge.Bridge.Record(key.ID, opts)
 	if err != nil {
 		fmt.Printf("failed to record. err: %s\r\n", err.Error())
-		return "",err
+		return "", err
 	}
 	r.Handle = hndl
 	return id, nil
@@ -107,25 +108,22 @@ func (r *Record) InitiateRecordingForChannel(channel *types.LineChannel) (string
 	id, err := r.createAPIResource()
 	if err != nil {
 		fmt.Printf("failed to record. err: %s", err.Error())
-		return "",err
+		return "", err
 	}
 
 	key := ari.NewKey(ari.LiveRecordingKey, id)
-	opts := &ari.RecordingOptions{ Format: "wav" }
+	opts := &ari.RecordingOptions{Format: "wav"}
 	hndl, err := channel.Channel.Record(key.ID, opts)
 	if err != nil {
 		fmt.Printf("failed to record. err: %s\r\n", err.Error())
-		return "",err
+		return "", err
 	}
-
 
 	r.Handle = hndl
 
 	return id, nil
 }
 
-
 func (r *Record) Stop() {
 	r.Handle.Stop()
 }
-
